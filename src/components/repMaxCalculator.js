0306aa7cd3c9a25a0f049249c2f5bcd8f4969830
Gordon Pham-Nguyen <gordon.pn6@gmail.com>
2025-05-03 22:52:58 -0400

    feat: calculate 1-15 reps ranges

--- src/components/repMaxCalculator.js
+++ src/components/repMaxCalculator.js
@@ -1,10 +1,12 @@
-// Basic Rep Max Calculator component
+// Rep Max Calculator component
 // Using the Epley formula: 1RM = w * (1 + r / 30)
+// And reverse: weight for n reps = 1RM / (1 + n / 30)
 
 export default () => ({
   weight: "",
   reps: "",
   estimatedMax: 0,
+  repRangeData: [],
   errorMessage: "",
 
   calculateRepMax() {
@@ -14,6 +16,7 @@ export default () => ({
 
     if (Number.isNaN(w) || w <= 0 || Number.isNaN(r) || r <= 0) {
       this.estimatedMax = 0;
+      this.repRangeData = [];
       if (this.weight || this.reps) {
         // Only show error if user started typing
         this.errorMessage =
@@ -26,10 +29,28 @@ export default () => ({
       this.estimatedMax = w; // If reps is 1, 1RM is the weight itself
     } else if (r > 30) {
       this.estimatedMax = 0;
+      this.repRangeData = [];
       this.errorMessage = "Calculation is less reliable for reps > 30.";
+      return;
     } else {
       // Epley formula
       this.estimatedMax = Math.round(w * (1 + r / 30));
+    }
+
+    // Calculate weights for rep ranges 1-15
+    this.repRangeData = [];
+    for (let reps = 1; reps <= 15; reps++) {
+      // Reverse Epley formula to get weight for given reps
+      const weightForReps = Math.round(this.estimatedMax / (1 + reps / 30));
+
+      // Calculate percentage of 1RM
+      const percentage = Math.round((weightForReps / this.estimatedMax) * 100);
+
+      this.repRangeData.push({
+        reps,
+        weight: weightForReps,
+        percentage,
+      });
     }
   },
 
